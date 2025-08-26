22e8c873975fbfd3499a727ca0286500cf06dff2
babygiraffe7 <trent-wilson7@live.com>
2025-08-26 10:57:38 -0500

    moving updates for src file

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/qc-dashboard2/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        pm: 'src/pmPage.html',
-      },
-    },
-  }
 });
