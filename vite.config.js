a5885681e55c251dfa9ae04ad6766e3c141b8063
babygiraffe7 <trent-wilson7@live.com>
2025-08-26 10:39:57 -0500

    adding rollupOptions in vite.config.js so the vite build can hopefully capture our pmPage.html file

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/qc-dashboard2/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        pm: 'src/pmPage.html',
+      },
+    },
+  }
 });
